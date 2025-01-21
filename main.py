487a816424f250ac92acc5f0d1b9f64999cca9be
Patricia-Almeida13 <patricia.s.almeida13@gmail.com>
2025-01-21 13:55:53 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Bom Dia !")
+nome = input("Indique o seu nome: ")
+print(f"Olá {nome}")
+
+idade = input("Qual a sua idade?")
+print(f"Tens {idade} anos")
+
+morada = input("Onde vive?")
+print(f"O seu adereço é {morada}")
