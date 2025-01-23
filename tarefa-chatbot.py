1b3e6d8a8d47000cbfc69d744138f935a7937cc7
Patricia-Almeida13 <patricia.s.almeida13@gmail.com>
2025-01-23 16:44:24 +0000

    Update tarefa-chatbot.py
    novas interações e correção de código

--- tarefa-chatbot.py
+++ tarefa-chatbot.py
@@ -3,8 +3,7 @@ from datetime import datetime
 
 def obter_resposta(texto: str) -> str:
     comando: str = texto.lower()
-    """""
-    if comando in ('olá', 'boa tarde', 'bom dia'):
+    """"" if comando in ('olá', 'boa tarde', 'bom dia'):
         return 'Olá tudo bem!'
     if comando == 'como estás':
         return 'Estou bem, obrigado!'
@@ -19,8 +18,7 @@ def obter_resposta(texto: str) -> str:
     if 'data' in comando:
         return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
 
-    return f'Desculpa, não entendi a questão! {texto}'
-"""
+    return f'Desculpa, não entendi a questão! {texto}' """
     respostas = {
         ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
         'como estás': 'Estou bem, obrigado!',
@@ -29,25 +27,28 @@ def obter_resposta(texto: str) -> str:
         'tempo': 'Está um dia de sol!',
         'horas': f'São: {datetime.now():%H:%M} horas',
         'data': f'Hoje é dia: {datetime.now():%d-%m-%Y}',
+        'quantos planetas existem': 'No nosso sistema solar, existem 8 planetas.',
+        'qual a tua cor favorita': 'A minha cor favorita é ...',
+        'o que és': 'sou um chatbot criado para te ajudar nas tuas duvidas',
+        'quantos ossos tem o corpo humano': 'O corpo humano adulto tem 206 ossos.',
+        'quem criou o python': 'o criador do python foi Guido van Rossum',
         ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
         'historia de portugal': 'Portugal tem uma rica história...',
     }
-
-for chave, resposta in respostas.items():
-    if isinstance(chave, tuple):
-        if comando in chave:
+    for chave, resposta in respostas.items():
+        if isinstance(chave, tuple):
+            if comando in chave:
+                return resposta
+        elif chave in comando:
             return resposta
-    elif chave in comando:
-        return resposta
 
-if 'horas' in comando:
-    return f'São: {datetime.now():%H:%M} horas'
+    if 'horas' in comando:
+        return f'São: {datetime.now():%H:%M} horas'
 
-if 'data' in comando:
-    return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
+    if 'data' in comando:
+        return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
 
-return f'Desculpa, não entendi a questão! {texto}'
-
+    return f'Desculpa, não entendi a questão! {texto}'
 
 
 def chat() -> None:
